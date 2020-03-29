87c90f1d78794b06ec0cd0836c57c1f1c44e8b95
thblt-thlgn <thibault@nobbas.com>
2020-03-29 17:54:40 -0400

    Add mutation loader

--- micro-services/libraries/@shared/utils/load-resolvers.ts
+++ micro-services/libraries/@shared/utils/load-resolvers.ts
@@ -5,9 +5,15 @@ export const loadResolvers = (dirname: string) => {
   const resolverList = fileLoader(path.join(dirname, './**/*.resolver.js'));
   const resolvers = resolverList.reduce((acc, func) => ({ ...acc, [func.name]: func }), {});
 
+  const mutationList = fileLoader(path.join(dirname, './**/*.mutation.js'));
+  const mutations = mutationList.reduce((acc, func) => ({ ...acc, [func.name]: func }), {});
+
   return {
     Query: {
       ...resolvers,
+    },
+    Mutation: {
+      ...mutations,
     },
   };
 };
