8072a852e6bfe5ab2064213ff87535839d959882
thblt-thlgn <thibault@nobbas.com>
2020-03-29 18:47:37 -0400

    Allow to load mutations

--- micro-services/libraries/@shared/utils/gql-schema-loader.ts
+++ micro-services/libraries/@shared/utils/gql-schema-loader.ts
@@ -2,16 +2,28 @@ import { fileLoader } from 'merge-graphql-schemas';
 import { GraphQLObjectType, GraphQLSchema } from 'graphql';
 import * as path from 'path';
 
-export const GQLSchemaLoader = (dirname: string) => {
+const loadQueries = (dirname: string): GraphQLObjectType => {
   const resolverList = fileLoader(path.join(dirname, './**/*.resolver.js'));
   const resolvers = resolverList.reduce((acc, resolver) => ({ ...acc, ...resolver }), {});
 
-  const Query = new GraphQLObjectType({
+  return new GraphQLObjectType({
     name: 'Query',
     fields: () => resolvers,
   });
+};
 
-  return new GraphQLSchema({
-    query: Query,
+const loadMutations = (dirname: string): GraphQLObjectType => {
+  const mutationList = fileLoader(path.join(dirname, './**/*.mutation.js'));
+  const mutations = mutationList.reduce((acc, mutation) => ({ ...acc, ...mutation }), {});
+
+  return new GraphQLObjectType({
+    name: 'Mutation',
+    fields: () => mutations,
   });
 };
+
+export const GQLSchemaLoader = (dirname: string) =>
+  new GraphQLSchema({
+    query: loadQueries(dirname),
+    mutation: loadMutations(dirname),
+  });
